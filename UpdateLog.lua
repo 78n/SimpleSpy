de836d528b5076da62192dddbdaf866855acca09
78n <108846385+78n@users.noreply.github.com>
2023-01-08 17:47:49 -0600

    Update UpdateLog.lua

--- UpdateLog.lua
+++ UpdateLog.lua
@@ -1,6 +1,5 @@
-local log = [[Update Logs:
+Update Logs:
 Rewrote generation to be a load faster
 Converted Simple Spy from hookfunction to hookmetamethod
 Added getupvalues to function info
-Changed function info generation so it is less performance heavy]]
-return log
+Changed function info generation so it is less performance heavy
