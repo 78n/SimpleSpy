adbd837618d78fe9a964e1be8bdae682fc013c0e
78n <108846385+78n@users.noreply.github.com>
2023-01-11 20:52:54 -0600

    Update UpdateLog.lua

--- UpdateLog.lua
+++ UpdateLog.lua
@@ -1,7 +1,13 @@
 Update Logs:
-Updated V3rmillion thread with Contact methods
-Rewrote mouseEntered
-Converted all BindToRenderStep's to RenderStepped connections
+Rewrote decompile so it does not decompile scripts that already have been decompiled
+Changed local varibles for buttons (Getting prepared to add config saving)
+When pressing Run Code if there is an error it will now display in the ToolTip
+Made the source code look worse (no problem happy to help)
+
+Contact Information:
+Contact me through the SimpleSpy discord (https://discord.com/invite/AWS6ez9) (post in general or something)
+or
+Reply to the v3rmillion thread (https://v3rmillion.net/showthread.php?tid=1198556)
 
 Extra Info:
 Wish engo a happy birthday
