d361613bee4af9efb6f66d8f5c85d32a59484183
78n <108846385+78n@users.noreply.github.com>
2023-01-13 10:19:43 -0600

    Update UpdateLog.lua

--- UpdateLog.lua
+++ UpdateLog.lua
@@ -1,7 +1,13 @@
 Update Logs:
+Fixed a bug where if you disabled function info it would make simple spy semi unusable until you renabled it (sorry)
+Changed any FindService to GetService because apparently there is a detection for FindService
+Fixed erroring when function info is enabled and you have a second version of simple spy running (more for personal reasons but it's a fix none the less)
+
+Yesterdays update log guh--[[
 Added config saving (I will start work on rewriting the highlight system)
 Made source code EVEN WORSE SOMEHOW
 report any errors to me (Amity) [Read contact information to reach me]
+]]
 
 Contact Information:
 Contact me through the SimpleSpy discord (https://discord.com/invite/AWS6ez9) (post in general or something)
