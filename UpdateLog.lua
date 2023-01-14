3582affa41181c61143bb460849a226a2dd4b3e1
78n <108846385+78n@users.noreply.github.com>
2023-01-14 17:46:37 -0600

    Update UpdateLog.lua

--- UpdateLog.lua
+++ UpdateLog.lua
@@ -1,17 +1,9 @@
 Update Logs:
-Fixed a bug where if you disabled function info it would make simple spy semi unusable until you renabled it (sorry)
-Changed any FindService to GetService because apparently there is a detection for FindService
-Fixed erroring when function info is enabled and you have a second version of simple spy running (more for personal reasons but it's a fix none the less)
-
-Yesterdays update log guh--[[
-Added config saving (I will start work on rewriting the highlight system)
-Made source code EVEN WORSE SOMEHOW
-report any errors to me (Amity) [Read contact information to reach me]
-]]
-
-Known Bugs:
-remotes fired index style will not have it's arguments logged/generated
-working on fix might have to resort back to hookfunction
+Fixed script's not generating fired index style
+if your executor does not support hookmetamethod SimpleSpy will only log remotes fired index style
+Now unhooks hookmetamethod
+Added Log metamethod by default it is set to false (useful when hooking remotes)
+Made the source look slightly cleaner :O
  
 Contact Information:
 Contact me through the SimpleSpy discord (https://discord.com/invite/AWS6ez9) (post in general or something)
@@ -19,4 +11,4 @@ or
 Reply to the v3rmillion thread (https://v3rmillion.net/showthread.php?tid=1198556)
 
 Extra Info:
-Thank you tip (tip#0145) for helping me testing
+Thank you tip (tip#0145) for helping me test last update
