07f47e9a69a5d17b67630d752a0257377b061191
78n <108846385+78n@users.noreply.github.com>
2023-01-16 14:52:53 -0600

    Update UpdateLog.lua

--- UpdateLog.lua
+++ UpdateLog.lua
@@ -1,8 +1,7 @@
 Update Logs:
-Rewrote the way script Simple Spy generates scripts
-To get more in detail with ^^^ I made it so that the script is only generated if the remote is selected
-This should improve the over all stability of Simple Spy AND generation speed
- 
+Rewrote stuff (i forgor what)
+Added Join Discord Button
+
 Contact Information:
 Contact me through the SimpleSpy discord (https://discord.com/invite/AWS6ez9) (post in general or something)
 or
@@ -10,4 +9,3 @@ Reply to the v3rmillion thread (https://v3rmillion.net/showthread.php?tid=119855
 
 Extra Info:
 If there are any errors report them to me (Amity)
-if there are any errors with the new script generation blame it on tip (tip#0145)
