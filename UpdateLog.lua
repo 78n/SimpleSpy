a8d25258664f76665895864007337ce51add1d42
78n <108846385+78n@users.noreply.github.com>
2023-01-19 18:37:19 -0600

    Update UpdateLog.lua

--- UpdateLog.lua
+++ UpdateLog.lua
@@ -1,6 +1,8 @@
 Update Logs:
-Patched Detection Method (Thank you https://v3rmillion.net/member.php?action=profile&uid=1397405)
-added a super secret button
+Added a more detailed error message (using roblox error system)
+Changed how run code functions (now uses Source.GenScript instead of the codebox's source)
+localized a lot of junk
+redid some stuff I cant remmber because it was during a nightime blur
 
 Contact Information:
 Contact me through the SimpleSpy discord (https://discord.com/invite/AWS6ez9) (post in general or something)
@@ -9,3 +11,4 @@ Reply to the v3rmillion thread (https://v3rmillion.net/showthread.php?tid=119855
 
 Extra Info:
 If there are any errors report them to me (Amity)
+Thanks tip for testing
