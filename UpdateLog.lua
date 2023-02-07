1b67e2af4938f0e96d579a9af6dc606978ba03dc
78n <108846385+78n@users.noreply.github.com>
2023-02-06 20:48:25 -0600

    Update UpdateLog.lua

--- UpdateLog.lua
+++ UpdateLog.lua
@@ -1,10 +1,8 @@
 Update Logs:
-Updated generation
-fixed generation not detecting when localplayer was fired (it was because of cloneref)
-changed Run Code to use the arguments the remote was fired with (no longer uses generated code)
-added return values to run code's tooltip
-fixed an error when selecting buttons
-fixed a bug where if you dragged the ui off screen it would be permanatly stuck to your cusor
+Dont worry Im still alive
+fixed a detectiong with __tostring
+rewrote parts of generation so it's more optimal (WIP)
+other stuff
 
 Contact Information:
 Contact me through the SimpleSpy discord (https://discord.com/invite/AWS6ez9) (post in general or something)
