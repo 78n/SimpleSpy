a86e3870cbbe8f251743c8fea3d2a35ad1599e76
78n <108846385+78n@users.noreply.github.com>
2023-02-17 23:11:48 -0600

    Update UpdateLog.lua

--- UpdateLog.lua
+++ UpdateLog.lua
@@ -1,16 +1,4 @@
 Update Logs:
-Dont worry Im still alive
-fixed a detectiong with __tostring
-rewrote parts of generation so it's more optimal (WIP)
-other stuff
-
-Contact Information:
-Contact me through the SimpleSpy discord (https://discord.com/invite/AWS6ez9) (post in general or something)
-or
-Reply to the v3rmillion thread (https://v3rmillion.net/showthread.php?tid=1198556)
-
-Extra Info:
-SIMPLE SPY V3 IS IN INFINITE YIELD
-If there are any errors report them to me (Amity)
-Thank you GameGuy
-Thanks tip for testing
+Mini update with stuff I will be doing a large/entire rewrite of stuff
+so do not expect any real updates any time soon I will work on making it more executor friendly
+and making it less detectable
