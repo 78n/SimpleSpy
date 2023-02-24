4d8e9173f86ec9e82d12a365f50e0e8277306e4d
78n <108846385+78n@users.noreply.github.com>
2023-02-23 21:09:49 -0600

    Update UpdateLog.lua

--- UpdateLog.lua
+++ UpdateLog.lua
@@ -1,4 +1,7 @@
 Update Logs:
-Mini update with stuff I will be doing a large/entire rewrite of stuff
-so do not expect any real updates any time soon I will work on making it more executor friendly
-and making it less detectable
+I have decided not to do a full rewrite
+  from scratch because I believe I may get
+  burned out if I do
+  but I will be doing an entire hooking rewrite to
+  hopefully optimize and secure simple spy v3
+  expect a little patch in an hour or so
