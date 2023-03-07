aa45384c27a0a296b60fcd2f9c0513bb2fb5ea42
78n <108846385+78n@users.noreply.github.com>
2023-03-06 22:11:11 -0600

    Update UpdateLog.lua

--- UpdateLog.lua
+++ UpdateLog.lua
@@ -1,4 +1,8 @@
 Update Logs:
-Doing some last bit testing
-I also added getcallbackmembers (syn v2 doesnt support getcallbackmember so no callbacks for you)
-and other stuff
+Finally updated this
+I've still got a lot to add but overall this should now work in a majority of games
+added callback stuff along with getconnections
+fixed a lot of detections
+fixed function info being disabled if a C closed function was passed
+I cut a lot of support because I kind of needed to add set thread identity
+and a lot of other stuff
