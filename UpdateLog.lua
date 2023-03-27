27a7a09bf3adcf37a4248d01b82b6adacab02d63
78n <108846385+78n@users.noreply.github.com>
2023-03-26 21:10:11 -0500

    Update UpdateLog.lua

--- UpdateLog.lua
+++ UpdateLog.lua
@@ -9,4 +9,3 @@ todo:
 rewrite highlight system (soon:tm:)
 table cloning (instead of weak tables)
 rewrite instance to string function (kinda scuffed right now)
-get toon to update infinite yield's rspy (IMPOSSIBLE)
