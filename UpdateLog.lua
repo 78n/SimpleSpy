5c75095351a9a706ae6b07ab19d51eb9ed5f5f5d
78n <108846385+78n@users.noreply.github.com>
2023-04-13 18:25:41 -0500

    Update UpdateLog.lua

--- UpdateLog.lua
+++ UpdateLog.lua
@@ -1,7 +1,9 @@
-Update:
-reverted update because apparently people didnt like the ui redesign
+Info update:
+Almost ready to update stuff
+I rewrote function info too and other junk
 
 todo:
+add return value logging
 rewrite highlight system (soon:tm:)
 table cloning (instead of weak tables)
 rewrite instance to string function (kinda scuffed right now)
