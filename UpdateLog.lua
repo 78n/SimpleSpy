688a7bfdaf86d8122e4454ec1376451b9d3e6fc8
78n <108846385+78n@users.noreply.github.com>
2023-04-28 22:00:31 -0500

    Update UpdateLog.lua

--- UpdateLog.lua
+++ UpdateLog.lua
@@ -1,9 +1,11 @@
 Update:
-Updated the beta link with the newest current version of simple spy
-no longer do you have to use weak tables it now has deepclone
-I havent tested this much due to the whole byfron situation
-so dont expect it to be very table
-next update I'll maybe try and add return value logging finally
+The major update is here
+Rewrote a lot (If you wanted to test it out early you could have used the beta link)
+Made a thread cache so after you close simple spy all most running threads are closed
+rewrote function info
+removed weaktables and made it so no args are lost
+patched detections
+and a lot more which I forgor
 
 Beta link:
 https://github.com/78n/SimpleSpy/blob/main/SimpleSpyBeta.lua
@@ -11,5 +13,3 @@ https://github.com/78n/SimpleSpy/blob/main/SimpleSpyBeta.lua
 todo:
 add return value logging
 rewrite highlight system (soon:tm:)
-table cloning (instead of weak tables)
-rewrite instance to string function (kinda scuffed right now)
