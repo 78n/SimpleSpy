737c5cb510b59475c4dc04e527105c744d4fc5de
Toon <85719086+Toon-arch@users.noreply.github.com>
2023-06-12 13:22:44 -0400

    Update UpdateLog.lua

--- UpdateLog.lua
+++ UpdateLog.lua
@@ -1,16 +1,1 @@
-Update:
-Development has paused while exploits are down
-
-To Celery users:
-fuck celery the functions are ass
-Added support to hookmetamethod but im not going to bother fixing hookfunction
-
-Beta link:
-https://github.com/78n/SimpleSpy/blob/main/SimpleSpyBeta.lua
-    
-Found a Bug?
-report it in the discord: https://discord.com/invite/AWS6ez9
-
-todo:
-add return value logging
-rewrite highlight system (soon:tm:)
+among us
