6be435a8d3c5dcbec3eb34da431a49863c87e294
78n <108846385+78n@users.noreply.github.com>
2023-06-15 15:18:56 -0500

    I forgor to update
    no scam

--- UpdateLog.lua
+++ UpdateLog.lua
@@ -1,9 +1,6 @@
 Update:
-Development has paused while exploits are down
-
-To Celery users:
-fuck celery the functions are ass
-Added support to hookmetamethod but im not going to bother fixing hookfunction
+Feature development has paused while exploits are down
+updated Beta version to add more support
 
 Beta link:
 https://github.com/78n/SimpleSpy/blob/main/SimpleSpyBeta.lua
