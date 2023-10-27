f9c965ec623290dbe74e90a5402f47baf921d5be
78n <108846385+78n@users.noreply.github.com>
2023-10-27 16:48:24 -0500

    Update UpdateLog.lua
    Discontinued

--- UpdateLog.lua
+++ UpdateLog.lua
@@ -1,15 +1,2 @@
-Current Update being developed on:
-I am almost done rewriting the system I am currently trying to squash a few rare bugs
-
-Update:
-Fixed an issue breaking simple spy
-
-Beta link:
-https://github.com/78n/SimpleSpy/blob/main/SimpleSpyBeta.lua
-    
-Found a Bug?
-report it in the discord: https://discord.com/invite/AWS6ez9
-
-todo:
-add return value logging (I'll make this before highlight system)
-rewrite highlight system (soon:tm:)
+SimpleSpy V3 is offically discontinued,
+Thank for using SimpleSpy V3 as your goto RemoteSpy
