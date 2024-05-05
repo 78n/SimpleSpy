ef3ba2f9b697e9b12011b4c4866cb8a0d11a29dd
78n <108846385+78n@users.noreply.github.com>
2024-05-04 22:13:23 -0500

    Update UpdateLog.lua

--- UpdateLog.lua
+++ UpdateLog.lua
@@ -1,2 +1,9 @@
-SimpleSpy V3 is offically discontinued,
-Thank for using SimpleSpy V3 as your goto RemoteSpy
+Hello my people to anyone that actually still uses Simple Spy Ive decided to start working on it again
+I've already fixed the biggest issue I had with simple spy... The syntax highligher
+As it is right now I can format the full Infinite Yield source in about 0.06~ seconds (roughly 450000~ characters)
+I'll likely make the default character limit about 100000 characters for the syntax and allow the user to configure their syntax to their preferance
+
+Along with that I'll start working on my userdata serializer again to hopefully make the output better and faster
+and last I'll be working towards adding Parrellel Lua support (Actors)
+TLDR:
+    Simple Spy V3 is being worked on again
