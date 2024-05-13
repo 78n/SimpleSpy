65f1c8e0f8a830054ce52dc136d661f102060876
78n <108846385+78n@users.noreply.github.com>
2024-05-12 21:25:39 -0500

    Update UpdateLog.lua

--- UpdateLog.lua
+++ UpdateLog.lua
@@ -1,3 +1,8 @@
+Rewrite Update:
+    I'm about like 50ish% done (I'm working on the hooking stuff right now)
+    Rewrite will have return logging for RemoteFunctions among other things
+    stay tune
+
 Hello my people to anyone that actually still uses Simple Spy Ive decided to start working on it again
 I've already fixed the biggest issue I had with simple spy... The syntax highligher
 As it is right now I can format the full Infinite Yield source in about 0.06~ seconds (roughly 450000~ characters)
